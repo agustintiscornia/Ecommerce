aaab08f51ef76096d2445bc3ebbdc886977e9f0b
Agustin <tiscornia.agustin@gmail.com>
2022-02-21 13:56:39 -0300

    navbar finalizado

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Navbar,Nav,} from 'react-bootstrap'
-
+import './styles/Nav.css'
 function AppNavbar() {
 return (
     <div>
-        <Navbar bg="Light" expand="lg">
+        <Navbar bg="dark" variant='blue' expand="lg">
     <Navbar.Brand className='logo' href="#home">Vixis</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="me-auto">
+    <Nav className="menu">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#link">Productos</Nav.Link>
         <Nav.Link href="#link">Nosotros</Nav.Link>
